1a3210c490a4ef2a41204edda397f7d6cb8fc12b
EdonGashi <edongashi@hotmail.co.uk>
2018-01-20 01:50:49 +0100

    Updated dialogs demo

--- src/Demo/Forge.Forms.Demo/Models/Dialogs.cs
+++ src/Demo/Forge.Forms.Demo/Models/Dialogs.cs
@@ -5,30 +5,39 @@ namespace Forge.Forms.Demo.Models
     [Title("Dialogs")]
     [Action("alert", "ALERT", InsertAfter = false)]
     [Action("confirm", "CONFIRM", InsertAfter = false)]
+    [Action("long_confirm", "LONG CONFIRM", InsertAfter = false)]
     [Action("prompt", "PROMPT", InsertAfter = false)]
     [Action("login", "LOGIN", InsertAfter = false)]
     [Divider]
     [Title("Windows")]
     [Action("alert", "ALERT", Parameter = "window", InsertAfter = false)]
     [Action("confirm", "CONFIRM", Parameter = "window", InsertAfter = false)]
+    [Action("long_confirm", "LONG CONFIRM", Parameter = "window", InsertAfter = false)]
     [Action("prompt", "PROMPT", Parameter = "window", InsertAfter = false)]
     [Action("login", "LOGIN", Parameter = "window", InsertAfter = false)]
     public class Dialogs : IActionHandler
     {
         public void HandleAction(object model, string action, object parameter)
         {
+            var longConfirm = new Confirm(
+                    "Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.",
+                    "Use Google's location service?", "TURN ON SPEED BOOST", "NO THANKS");
+
             if (parameter is "window")
             {
                 switch (action)
                 {
                     case "alert":
-                        Show.Window().For(new Alert("Hello world!"));
+                        Show.Window(275d).For(new Alert("Hello world!"));
                         break;
                     case "confirm":
-                        Show.Window().For(new Confirm("Delete item?"));
+                        Show.Window(275d).For(new Confirm("Delete item?"));
+                        break;
+                    case "long_confirm":
+                        Show.Window(250d).For(longConfirm);
                         break;
                     case "prompt":
-                        Show.Window().For(new Prompt<string> { Title = "What's your name?" });
+                        Show.Window().For(new Prompt<string> { Title = "What's your name?", Value = "User" });
                         break;
                     case "login":
                         Show.Window().For<Login>();
@@ -42,13 +51,16 @@ namespace Forge.Forms.Demo.Models
                 switch (action)
                 {
                     case "alert":
-                        Show.Dialog().For(new Alert("Hello world!"));
+                        Show.Dialog(275d).For(new Alert("Hello world!"));
                         break;
                     case "confirm":
-                        Show.Dialog().For(new Confirm("Delete item?"));
+                        Show.Dialog(275d).For(new Confirm("Delete item?"));
+                        break;
+                    case "long_confirm":
+                        Show.Dialog(250d).For(longConfirm);
                         break;
                     case "prompt":
-                        Show.Dialog().For(new Prompt<string> { Title = "What's your name?" });
+                        Show.Dialog().For(new Prompt<string> { Title = "What's your name?", Value = "User" });
                         break;
                     case "login":
                         Show.Dialog().For<Login>();
