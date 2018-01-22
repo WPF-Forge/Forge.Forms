7583d169534d5d7475e0bbceb2d8bb5315a3b883
EdonGashi <edongashi@hotmail.co.uk>
2018-01-22 20:32:55 +0100

    Updated user demo actions

--- src/Demo/Forge.Forms.Demo/Models/User.cs
+++ src/Demo/Forge.Forms.Demo/Models/User.cs
@@ -5,8 +5,8 @@ using MaterialDesignThemes.Wpf;
 namespace Forge.Forms.Demo.Models
 {
     [Title("Create account")]
-    [Action("cancel", "CANCEL")]
-    [Action("register", "REGISTER")]
+    [Action("cancel", "CANCEL", IsReset = true, IsCancel = true)]
+    [Action("register", "REGISTER", Validates = true, IsDefault = true)]
     public class User
     {
         [Heading("Personal details")]
