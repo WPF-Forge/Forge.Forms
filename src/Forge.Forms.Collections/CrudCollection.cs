b23490baeda621838defb429aadead419a0bb4e3
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-01-23 17:45:47 -0200

    Added basic collection implementation

--- src/Forge.Forms.Collections/CrudCollection.cs
+++ src/Forge.Forms.Collections/CrudCollection.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Forge.Forms.Collections
 {
-    public class CrudCollection<T> : IList<T>, INotifyCollectionChanged
+    public class CrudCollection<T> : IList<T>, INotifyCollectionChanged, INotifyPropertyChanged
     {
-        private List<T> InternalCollection { get; set; }
+        private List<T> InternalCollection { get; } = new List<T>();
 
         public CrudCollection()
         {
@@ -14,7 +18,7 @@ namespace Forge.Forms.Collections
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return InternalCollection.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -25,29 +29,50 @@ namespace Forge.Forms.Collections
         public void Add(T item)
         {
             InternalCollection.Add(item);
+
+            if (item is INotifyPropertyChanged itemNotifyPropertyChanged)
+            {
+                itemNotifyPropertyChanged.PropertyChanged += ItemNotifyPropertyChangedOnPropertyChanged;
+            }
+
             OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item));
+        }
+
+        private void ItemNotifyPropertyChangedOnPropertyChanged(object sender,
+            PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            this.OnPropertyChanged("Count");
+            this.OnPropertyChanged("Item[]");
         }
 
         public void Clear()
         {
             OnCollectionChanged(
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, InternalCollection));
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
             InternalCollection.Clear();
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            return InternalCollection.Contains(item);
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new System.NotImplementedException();
+            InternalCollection.CopyTo(array, arrayIndex);
         }
 
         public bool Remove(T item)
         {
-            throw new System.NotImplementedException();
+            if (item is INotifyPropertyChanged itemNotifyPropertyChanged)
+            {
+                itemNotifyPropertyChanged.PropertyChanged -= ItemNotifyPropertyChangedOnPropertyChanged;
+            }
+
+            var removeResult = InternalCollection.Remove(item);
+            OnCollectionChanged(
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item));
+            return removeResult;
         }
 
         public int Count { get; }
@@ -55,23 +80,25 @@ namespace Forge.Forms.Collections
 
         public int IndexOf(T item)
         {
-            throw new System.NotImplementedException();
+            return InternalCollection.IndexOf(item);
         }
 
         public void Insert(int index, T item)
         {
-            throw new System.NotImplementedException();
+            InternalCollection.Insert(index, item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
         }
 
         public void RemoveAt(int index)
         {
-            throw new System.NotImplementedException();
+            var item = InternalCollection[index];
+            Remove(item);
         }
 
         public T this[int index]
         {
-            get { throw new System.NotImplementedException(); }
-            set { throw new System.NotImplementedException(); }
+            get => InternalCollection[index];
+            set => InternalCollection[index] = value;
         }
 
         public event NotifyCollectionChangedEventHandler CollectionChanged;
@@ -79,6 +106,13 @@ namespace Forge.Forms.Collections
         protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
             CollectionChanged?.Invoke(this, e);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
