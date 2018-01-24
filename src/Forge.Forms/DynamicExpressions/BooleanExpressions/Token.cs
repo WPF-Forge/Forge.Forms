177dd07f735710ea67a49cb79726de003cde1aba
EdonGashi <edongashi@hotmail.co.uk>
2018-01-24 01:01:06 +0100

    Added tokens

---
+++ src/Forge.Forms/DynamicExpressions/BooleanExpressions/Token.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Forge.Forms.DynamicExpressions.BooleanExpressions
+{
+    internal abstract class Token
+    {
+        public Token[] Parse(string input)
+        {
+            var result = new List<Token>();
+            using (var reader = new StringReader(input))
+            {
+                throw new NotImplementedException();
+            }
+
+            return result.ToArray();
+        }
+    }
+
+    internal class AndToken : Token
+    {
+    }
+
+    internal class OrToken : Token
+    {
+    }
+
+    internal class NotToken : Token
+    {
+    }
+
+    internal class ValueToken : Token
+    {
+        public int Index { get; set; }
+    }
+}
