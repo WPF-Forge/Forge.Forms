a9cf83721a1c68015018b79f6a5b84d1d6968648
EdonGashi <edongashi@hotmail.co.uk>
2018-01-26 19:54:08 +0100

    Added DynamicDataGrid dialog properties and command bindings

--- src/Forge.Forms.Collections/DynamicDataGrid.cs
+++ src/Forge.Forms.Collections/DynamicDataGrid.cs
@@ -6,6 +6,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Forge.Forms.DynamicExpressions;
 using Forge.Forms.FormBuilding;
 using Forge.Forms.FormBuilding.Defaults;
@@ -14,95 +15,241 @@ using Expression = System.Linq.Expressions.Expression;
 
 namespace Forge.Forms.Collections
 {
-
-
     public class DynamicDataGrid : Control
     {
-        public static readonly DependencyProperty SaveActionContentProperty = DependencyProperty.Register(
-            nameof(SaveActionContent),
+        public static readonly DependencyProperty CreateDialogPositiveContentProperty =
+            DependencyProperty.Register(
+                nameof(CreateDialogPositiveContent),
+                typeof(string),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata("ADD"));
+
+        public string CreateDialogPositiveContent
+        {
+            get => (string)GetValue(CreateDialogPositiveContentProperty);
+            set => SetValue(CreateDialogPositiveContentProperty, value);
+        }
+
+        public static readonly DependencyProperty CreateDialogPositiveIconProperty =
+            DependencyProperty.Register(
+                nameof(CreateDialogPositiveIcon),
+                typeof(PackIconKind?),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(PackIconKind.Check));
+
+        public PackIconKind? CreateDialogPositiveIcon
+        {
+            get => (PackIconKind)GetValue(CreateDialogPositiveIconProperty);
+            set => SetValue(CreateDialogPositiveIconProperty, value);
+        }
+
+        public static readonly DependencyProperty CreateDialogNegativeContentProperty = DependencyProperty.Register(
+            nameof(CreateDialogNegativeContent),
             typeof(string),
             typeof(DynamicDataGrid),
-            new FrameworkPropertyMetadata("Save"));
+            new FrameworkPropertyMetadata("CANCEL"));
 
-        public string SaveActionContent
+        public string CreateDialogNegativeContent
         {
-            get => (string)GetValue(SaveActionContentProperty);
-            set => SetValue(SaveActionContentProperty, value);
+            get => (string)GetValue(CreateDialogNegativeContentProperty);
+            set => SetValue(CreateDialogNegativeContentProperty, value);
         }
 
-        public static readonly DependencyProperty SaveAcitionIconProperty = DependencyProperty.Register(
-            nameof(SaveAcitionIcon),
-            typeof(PackIconKind),
-            typeof(DynamicDataGrid),
-            new FrameworkPropertyMetadata(PackIconKind.ContentSave));
+        public static readonly DependencyProperty CreateDialogNegativeIconProperty =
+            DependencyProperty.Register(
+                nameof(CreateDialogNegativeIcon),
+                typeof(PackIconKind?),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(PackIconKind.Close));
 
-        public PackIconKind SaveAcitionIcon
+        public PackIconKind? CreateDialogNegativeIcon
         {
-            get => (PackIconKind)GetValue(SaveAcitionIconProperty);
-            set => SetValue(SaveAcitionIconProperty, value);
+            get => (PackIconKind)GetValue(CreateDialogNegativeIconProperty);
+            set => SetValue(CreateDialogNegativeIconProperty, value);
         }
 
-        public static readonly DependencyProperty CancelActionContentProperty = DependencyProperty.Register(
-            nameof(CancelActionContent),
+        public static readonly DependencyProperty EditDialogPositiveContentProperty = DependencyProperty.Register(
+            nameof(EditDialogPositiveContent),
             typeof(string),
             typeof(DynamicDataGrid),
-            new FrameworkPropertyMetadata("Cancel"));
+            new FrameworkPropertyMetadata("SAVE"));
 
-        public string CancelActionContent
+        public string EditDialogPositiveContent
         {
-            get => (string)GetValue(CancelActionContentProperty);
-            set => SetValue(CancelActionContentProperty, value);
+            get => (string)GetValue(EditDialogPositiveContentProperty);
+            set => SetValue(EditDialogPositiveContentProperty, value);
         }
 
-        public static readonly DependencyProperty CancelActionIconProperty = DependencyProperty.Register(
-            nameof(CancelActionIcon),
-            typeof(PackIconKind),
-            typeof(DynamicDataGrid),
-            new FrameworkPropertyMetadata(PackIconKind.Close));
+        public static readonly DependencyProperty EditDialogPositiveIconProperty =
+            DependencyProperty.Register(
+                nameof(EditDialogPositiveIcon),
+                typeof(PackIconKind?),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(PackIconKind.Check));
 
-        public PackIconKind CancelActionIcon
+        public PackIconKind? EditDialogPositiveIcon
         {
-            get => (PackIconKind)GetValue(CancelActionIconProperty);
-            set => SetValue(CancelActionIconProperty, value);
+            get => (PackIconKind)GetValue(EditDialogPositiveIconProperty);
+            set => SetValue(EditDialogPositiveIconProperty, value);
         }
 
-        public static readonly DependencyProperty UpdateActionContentProperty = DependencyProperty.Register(
-            nameof(UpdateActionContent),
+        public static readonly DependencyProperty EditDialogNegativeContentProperty = DependencyProperty.Register(
+            nameof(EditDialogNegativeContent),
             typeof(string),
             typeof(DynamicDataGrid),
-            new FrameworkPropertyMetadata("Edit"));
+            new FrameworkPropertyMetadata("CANCEL"));
 
-        public string UpdateActionContent
+        public string EditDialogNegativeContent
         {
-            get => (string)GetValue(UpdateActionContentProperty);
-            set => SetValue(UpdateActionContentProperty, value);
+            get => (string)GetValue(EditDialogNegativeContentProperty);
+            set => SetValue(EditDialogNegativeContentProperty, value);
         }
 
-        public static readonly DependencyProperty UpdateActionIconProperty = DependencyProperty.Register(
-            nameof(UpdateActionIcon),
-            typeof(PackIconKind),
-            typeof(DynamicDataGrid),
-            new FrameworkPropertyMetadata(PackIconKind.Pencil));
+        public static readonly DependencyProperty EditDialogNegativeIconProperty =
+            DependencyProperty.Register(
+                nameof(EditDialogNegativeIcon),
+                typeof(PackIconKind?),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(PackIconKind.Close));
 
-        public PackIconKind UpdateActionIcon
+        public PackIconKind? EditDialogNegativeIcon
         {
-            get => (PackIconKind)GetValue(UpdateActionIconProperty);
-            set => SetValue(UpdateActionIconProperty, value);
+            get => (PackIconKind)GetValue(EditDialogNegativeIconProperty);
+            set => SetValue(EditDialogNegativeIconProperty, value);
+        }
+
+        public static readonly DependencyProperty RemoveDialogPositiveContentProperty = DependencyProperty.Register(
+            nameof(RemoveDialogPositiveContent),
+            typeof(string),
+            typeof(DynamicDataGrid),
+            new FrameworkPropertyMetadata("REMOVE"));
+
+        public string RemoveDialogPositiveContent
+        {
+            get => (string)GetValue(RemoveDialogPositiveContentProperty);
+            set => SetValue(RemoveDialogPositiveContentProperty, value);
+        }
+
+        public static readonly DependencyProperty RemoveDialogPositiveIconProperty =
+            DependencyProperty.Register(
+                nameof(RemoveDialogPositiveIcon),
+                typeof(PackIconKind?),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(PackIconKind.Delete));
+
+        public PackIconKind? RemoveDialogPositiveIcon
+        {
+            get => (PackIconKind)GetValue(RemoveDialogPositiveIconProperty);
+            set => SetValue(RemoveDialogPositiveIconProperty, value);
+        }
+
+        public static readonly DependencyProperty RemoveDialogNegativeContentProperty = DependencyProperty.Register(
+            nameof(RemoveDialogNegativeContent),
+            typeof(string),
+            typeof(DynamicDataGrid),
+            new FrameworkPropertyMetadata("CANCEL"));
+
+        public string RemoveDialogNegativeContent
+        {
+            get => (string)GetValue(RemoveDialogNegativeContentProperty);
+            set => SetValue(RemoveDialogNegativeContentProperty, value);
+        }
+
+        public static readonly DependencyProperty RemoveDialogNegativeIconProperty =
+            DependencyProperty.Register(
+                nameof(RemoveDialogNegativeIcon),
+                typeof(PackIconKind?),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(PackIconKind.Close));
+
+        public PackIconKind? RemoveDialogNegativeIcon
+        {
+            get => (PackIconKind)GetValue(RemoveDialogNegativeIconProperty);
+            set => SetValue(RemoveDialogNegativeIconProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(
+                nameof(ItemsSource),
+                typeof(IEnumerable),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata());
+
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty DialogOptionsProperty =
+            DependencyProperty.Register(
+                nameof(DialogOptions),
+                typeof(DialogOptions),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata(DialogOptions.Default));
+
+        public DialogOptions DialogOptions
+        {
+            get => (DialogOptions)GetValue(DialogOptionsProperty);
+            set => SetValue(DialogOptionsProperty, value);
+        }
+
+        public static readonly DependencyProperty TargetDialogIdentifierProperty =
+            DependencyProperty.Register(
+                nameof(TargetDialogIdentifier),
+                typeof(object),
+                typeof(DynamicDataGrid),
+                new FrameworkPropertyMetadata());
+
+        public object TargetDialogIdentifier
+        {
+            get => GetValue(TargetDialogIdentifierProperty);
+            set => SetValue(TargetDialogIdentifierProperty, value);
         }
 
         private static readonly Dictionary<Type, Action<object, object>> AddItemCache =
             new Dictionary<Type, Action<object, object>>();
 
-        // TODO: Dependency properties
-        /// <summary>
-        /// The items source.
-        /// </summary>
-        public IEnumerable ItemsSource { get; set; }
+        public static RoutedCommand AddItemCommand = new RoutedCommand();
+        public static RoutedCommand EditItemCommand = new RoutedCommand();
+        public static RoutedCommand RemoveItemCommand = new RoutedCommand();
 
-        /// <summary>
-        /// The dialog identifier in which the input dialogs should be displayed.
-        /// </summary>
-        public object TargetDialogIdentifier { get; set; }
+        public DynamicDataGrid()
+        {
+            CommandBindings.Add(new CommandBinding(AddItemCommand, ExecuteAddItem, CanExecuteAddItem));
+            CommandBindings.Add(new CommandBinding(EditItemCommand, ExecuteEditItem, CanExecuteEditItem));
+            CommandBindings.Add(new CommandBinding(AddItemCommand, ExecuteDeleteItem, CanExecuteDeleteItem));
+        }
+
+        private void ExecuteAddItem(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CanExecuteAddItem(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ItemsSource != null;
+        }
+
+        private void ExecuteEditItem(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CanExecuteEditItem(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ItemsSource != null;
+        }
+
+        private void ExecuteDeleteItem(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CanExecuteDeleteItem(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ItemsSource != null;
+        }
 
         private void RefreshItemsView()
         {
@@ -257,7 +404,8 @@ namespace Forge.Forms.Collections
 
     internal class FormDefinitionWrapper : IFormDefinition
     {
-        public FormDefinitionWrapper(IReadOnlyList<FormRow> formRows, double[] grid, Type modelType, IDictionary<string, IValueProvider> resources)
+        public FormDefinitionWrapper(IReadOnlyList<FormRow> formRows, double[] grid, Type modelType,
+            IDictionary<string, IValueProvider> resources)
         {
             FormRows = formRows;
             Grid = grid;
--- src/Forge.Forms/Snapshot.cs
+++ src/Forge.Forms/Snapshot.cs
@@ -1,135 +1,58 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using FastMember;
 
 namespace Forge.Forms
 {
-    /// <inheritdoc />
-    /// <summary>
-    ///     Generic version of snapshot
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <seealso cref="T:Forge.Forms.Snapshot" />
-    public class Snapshot<T> : Snapshot
+    public sealed class Snapshot
     {
-        /// <summary>
-        /// Gets the object.
-        /// </summary>
-        /// <value>
-        /// The object.
-        /// </value>
-        public new T Object
+        private readonly Dictionary<string, object> values;
+
+        public Snapshot(object model, HashSet<string> properties)
         {
-            get
+            SnapshotTime = DateTime.Now;
+            ObjectType = model.GetType();
+            values = new Dictionary<string, object>();
+            var reader = ObjectAccessor.Create(model);
+            foreach (var property in properties)
             {
-                var obj = ((Snapshot) this).Object;
-                if (!(obj is IList objList)) return (T) obj;
-                var method = typeof(Enumerable).GetMethod("OfType");
-                if (method == null) return (T) obj;
-
-                var result = (IEnumerable<object>) method.MakeGenericMethod(EnumerableType).Invoke
-                    (null, new object[] {objList});
-
-                if (!(Activator.CreateInstance(typeof(T)) is IList newList))
-                    return (T)obj;
-
-                foreach (var o in result.ToArray())
+                object value = null;
+                try
                 {
-                    newList.Add(o);
+                    value = reader[property];
+                }
+                catch
+                {
+                    continue;
                 }
 
-                return (T) newList;
+                values[property] = value;
             }
         }
 
-        /// <inheritdoc />
-        public Snapshot(T o) : base(o)
+        public DateTime SnapshotTime { get; }
+
+        public Type ObjectType { get; }
+
+        public void Apply(object model)
         {
-        }
-    }
-
-
-    /// <summary>
-    ///     Stores an object in its current state
-    /// </summary>
-    public class Snapshot
-    {
-        /// <inheritdoc />
-        public Snapshot(object o)
-        {
-            Object = o ?? throw new ArgumentNullException(nameof(o));
-
-            if (!HasParameterlessConstructor())
-                throw new Exception($"Type {SnapshotType.Name} does not contain a parameterless constructor");
-
-            Object = CreateSnapshot(o);
-        }
-
-        /// <summary>
-        ///     Gets the type of the snapshot.
-        /// </summary>
-        /// <value>
-        ///     The type of the snapshot.
-        /// </value>
-        public Type SnapshotType => Object.GetType();
-
-        /// <summary>
-        /// Gets the type of the enumerable. Null if the object is not a enumerable.
-        /// </summary>
-        /// <value>
-        /// The type of the enumerable.
-        /// </value>
-        public Type EnumerableType { get; private set; }
-
-        /// <summary>
-        ///     Gets the object.
-        /// </summary>
-        /// <value>
-        ///     The object.
-        /// </value>
-        public object Object { get; }
-
-        private List<object> CreateSnapshot(IEnumerable o, Type enumerableType)
-        {
-            return o.OfType<object>().Select(CreateSnapshot).ToList();
-        }
-
-        private object CreateSnapshot(object o)
-        {
-            if (o is IEnumerable enumerable)
+            if (model == null || model.GetType() != ObjectType)
             {
-                var genericArguments = GetEnumerableGenericArguments(o);
-
-                if (genericArguments.Length <= 0)
-                    throw new Exception($"Couldnt determine the type of an enumerable. {enumerable}");
-
-                EnumerableType = genericArguments.First();
-                return CreateSnapshot(enumerable, EnumerableType);
+                throw new ArgumentException("Invalid model.");
             }
 
-            return FastDeepCloner.DeepCloner.Clone(o);
-        }
-
-        /// <summary>
-        /// Gets the enumerable generic arguments.
-        /// </summary>
-        /// <param name="o">The o.</param>
-        /// <returns></returns>
-        public static Type[] GetEnumerableGenericArguments(object o)
-        {
-            return o.GetType().GetGenericArguments();
-        }
-
-        /// <summary>
-        ///     Determines whether [has parameterless constructor].
-        /// </summary>
-        /// <returns>
-        ///     <c>true</c> if [has parameterless constructor]; otherwise, <c>false</c>.
-        /// </returns>
-        private bool HasParameterlessConstructor()
-        {
-            return SnapshotType.GetConstructor(Type.EmptyTypes) != null;
+            var setter = ObjectAccessor.Create(model);
+            foreach (var kvp in values)
+            {
+                try
+                {
+                    setter[kvp.Key] = kvp.Value;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
         }
     }
 }
