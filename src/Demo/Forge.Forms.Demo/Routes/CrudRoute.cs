a95ce25ae94c2b7478fd45666c0dc5daf36f935a
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-02-06 21:26:02 -0200

    Added entity framework core support to crud demo

--- src/Demo/Forge.Forms.Demo/Routes/CrudRoute.cs
+++ src/Demo/Forge.Forms.Demo/Routes/CrudRoute.cs
@@ -1,24 +1,90 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Forge.Forms.Annotations;
+using Forge.Forms.Collections;
+using Forge.Forms.Collections.Interfaces;
 using Material.Application.Routing;
 using MaterialDesignThemes.Wpf;
+using Microsoft.EntityFrameworkCore;
 
 namespace Forge.Forms.Demo.Routes
 {
+    class CrudExample : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=crud.db");
+        }
+    }
+
+    class CrudInterceptor : IAddActionInterceptor, IUpdateActionInterceptor, IRemoveActionInterceptor
+    {
+        private CrudExample Context { get; }
+
+        public CrudInterceptor(CrudExample context)
+        {
+            Context = context;
+        }
+
+        public IAddActionContext Intercept(IAddActionContext modelContext)
+        {
+            Context.Add((Person)modelContext.NewModel);
+            Context.SaveChanges();
+            return null;
+        }
+
+        public IUpdateActionContext Intercept(IUpdateActionContext modelContext)
+        {
+            if (modelContext.NewModel is Person person)
+            {
+                Context.Entry(person).State = EntityState.Modified;
+                Context.SaveChanges();
+            }
+
+            return modelContext;
+        }
+
+        public void Intercept(IRemoveActionContext modelContext)
+        {
+
+            if (modelContext.OldModel is Person person)
+            {
+                Context.Remove(person);
+                Context.SaveChanges();
+            }
+        }
+    }
+
     public class CrudRoute : Route
     {
-        public List<Person> Items { get; } = new List<Person>();
+        public ObservableCollection<Person> Items { get; }
 
         public CrudRoute()
         {
             RouteConfig.Title = "Crud examples";
             RouteConfig.Icon = PackIconKind.Table;
+
+            var context = new CrudExample();
+            context.Database.EnsureCreated();
+
+            context.Persons.Load();
+            Items = context.Persons.Local.ToObservableCollection();
+
+            var interceptor = new CrudInterceptor(context);
+            DynamicDataGrid.AddInterceptorChain.Add(interceptor);
+            DynamicDataGrid.UpdateInterceptorChain.Add(interceptor);
+            DynamicDataGrid.RemoveInterceptorChain.Add(interceptor);
         }
     }
 
     public class Person : INotifyPropertyChanged
     {
+        [FieldIgnore]
+        public int Id { get; set; }
+
         private string firstName;
         private string lastName;
 
