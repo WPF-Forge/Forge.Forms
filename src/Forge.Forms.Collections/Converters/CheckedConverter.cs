927babd5f663bc7612d2a0d4c445d762982a1f85
redbaty <juninhowblu@gmail.com>
2018-02-15 09:37:59 -0200

    Added CheckedConverter

---
+++ src/Forge.Forms.Collections/Converters/CheckedConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Forge.Forms.Collections.Converters
+{
+    public class CheckedConverter : IMultiValueConverter
+    {
+        private static Dictionary<DynamicDataGrid, Dictionary<object, bool>> CheckedItems { get; } =
+            new Dictionary<DynamicDataGrid, Dictionary<object, bool>>();
+
+        internal static bool IsChecked(DynamicDataGrid grid, object model)
+        {
+            var i = GetItems(grid);
+            if (!i.ContainsKey(model))
+                i.Add(model, false);
+
+            return i[model];
+        }
+
+        internal static bool SetChecked(DynamicDataGrid grid, object model, bool state)
+        {
+            var i = GetItems(grid);
+            if (!i.ContainsKey(model))
+                i.Add(model, false);
+
+            i[model] = state;
+            grid.OnPropertyChanged("SelectedItems");
+            return i[model];
+        }
+
+        internal static Dictionary<object, bool> GetItems(DynamicDataGrid grid)
+        {
+            if (!CheckedItems.ContainsKey(grid))
+                CheckedItems.Add(grid, new Dictionary<object, bool>());
+
+            return CheckedItems[grid];
+        }
+
+        /// <inheritdoc />
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is CheckBox && values[1] is DataGridRow row && parameter is DynamicDataGrid dataGrid)
+            {
+                var items = GetItems(dataGrid);
+
+                if (!items.ContainsKey(row.Item))
+                    items.Add(row.Item, false);
+
+                dataGrid.UpdateHeaderButton();
+
+                return items[row.Item];
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new[] { value };
+        }
+    }
+}
