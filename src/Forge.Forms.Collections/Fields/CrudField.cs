ae6ec7207148f197bbb890eb5830a8b8545e787c
EdonGashi <edongashi@hotmail.co.uk>
2018-02-25 23:44:22 +0100

    Initial skeleton for crud field

---
+++ src/Forge.Forms.Collections/Fields/CrudField.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Forge.Forms.FormBuilding;
+
+namespace Forge.Forms.Collections.Fields
+{
+    public class CrudField : DataFormField
+    {
+        public CrudField(string key, Type propertyType)
+            : base(key, propertyType)
+        {
+        }
+
+        protected override IBindingProvider CreateBindingProvider(IResourceContext context, IDictionary<string, IValueProvider> formResources)
+        {
+            return new CrudPresenter(context, Resources, formResources);
+        }
+    }
+
+    public class CrudPresenter : ValueBindingProvider
+    {
+        static CrudPresenter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CrudPresenter),
+                new FrameworkPropertyMetadata(typeof(CrudPresenter)));
+        }
+
+        public CrudPresenter(IResourceContext context,
+            IDictionary<string, IValueProvider> fieldResources,
+            IDictionary<string, IValueProvider> formResources)
+            : base(context, fieldResources, formResources, true)
+        {
+        }
+    }
+}
---
+++ src/Forge.Forms.Collections/Fields/CrudFieldBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Forge.Forms.Collections.Annotations;
+using Forge.Forms.FormBuilding;
+
+namespace Forge.Forms.Collections.Fields
+{
+    class CrudFieldBuilder : IFieldBuilder
+    {
+        public FormElement TryBuild(IFormProperty property, Func<string, object> deserializer)
+        {
+            if (property.GetCustomAttribute<CrudAttribute>() != null)
+            {
+                return new CrudField(property.Name, property.PropertyType);
+            }
+
+            return null;
+        }
+    }
+}
