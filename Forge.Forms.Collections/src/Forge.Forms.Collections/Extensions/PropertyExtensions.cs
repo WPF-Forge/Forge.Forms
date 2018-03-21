2ced81a2a4fdd9d77d7c1a5203b711aafb861904
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-03-20 21:13:08 -0300

    Added PropertyExtensions

---
+++ Forge.Forms.Collections/src/Forge.Forms.Collections/Extensions/PropertyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Windows.Data;
+
+namespace Forge.Forms.Collections.Extensions
+{
+    internal static class PropertyExtensions
+    {
+        internal static Binding CreateBinding(this PropertyInfo propertyInfo, string path = null)
+        {
+            return new Binding(string.IsNullOrEmpty(path) ? propertyInfo.Name : path)
+            {
+                Mode = propertyInfo.CanRead && propertyInfo.CanWrite
+                    ? BindingMode.TwoWay
+                    : BindingMode.Default,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+        }
+    }
+}
