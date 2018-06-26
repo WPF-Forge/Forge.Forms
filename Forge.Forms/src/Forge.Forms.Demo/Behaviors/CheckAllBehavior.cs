f69cab0a03b7d923e0a4607d892f48891d295873
Edon Gashi <edon.gashi@uni-pr.edu>
2018-06-26 17:06:02 +0200

    Improved checkall behavior

--- Forge.Forms/src/Forge.Forms.Demo/Behaviors/CheckAllBehavior.cs
+++ Forge.Forms/src/Forge.Forms.Demo/Behaviors/CheckAllBehavior.cs
@@ -1,40 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Markup;
 using Forge.Forms.Behaviors;
 using Forge.Forms.FormBuilding;
 
 namespace Forge.Forms.Demo.Behaviors
 {
-    public class CheckAllBehavior : IPropertyChangedBehavior
+    public class CheckAllBehavior : IModelChangedBehavior, IPropertyChangedBehavior
     {
+        private Dictionary<string, string[]> parentChildRelationships = new Dictionary<string, string[]>();
+        private Dictionary<string, string> childParentRelationships = new Dictionary<string, string>();
+        private bool locked;
+
         public void PropertyChanged(IPropertyChangedContext context)
         {
+            if (locked)
+            {
+                return;
+            }
+
             if (!(context.Model is IDictionary<string, object> model))
             {
                 return;
             }
 
-            // Select all only on checked.
-            if (model[context.PropertyName] is bool b)
-            {
-                var meta = context
-                    .FormDefinition
-                    .GetElements()
-                    .FirstOrDefault(elem => elem is DataFormField d && d.Key == context.PropertyName)
-                    ?.Metadata;
+            locked = true;
 
-                if (meta == null || !meta.TryGetValue("sets", out var value))
+            var propertyName = context.PropertyName;
+            var value = model[propertyName];
+            if (parentChildRelationships.TryGetValue(propertyName, out var children))
+            {
+                if (value is bool b)
                 {
-                    return;
+                    foreach (var child in children)
+                    {
+                        model[child] = b;
+                    }
+                }
+            }
+            else if (childParentRelationships.TryGetValue(propertyName, out var parent))
+            {
+                children = parentChildRelationships[parent];
+                var areEqual = true;
+                foreach (var child in children)
+                {
+                    if (!Equals(model[child], value))
+                    {
+                        areEqual = false;
+                        break;
+                    }
                 }
 
-                var props = value?.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(v => v.Trim());
-                if (props != null)
+                model[parent] = areEqual ? value : null;
+            }
+
+            locked = false;
+        }
+
+        public void ModelChanged(IEventContext context)
+        {
+            childParentRelationships = new Dictionary<string, string>();
+            parentChildRelationships = new Dictionary<string, string[]>();
+            locked = false;
+            if (context.FormDefinition != null)
+            {
+                foreach (var element in context.FormDefinition.GetElements())
                 {
-                    foreach (var prop in props)
+                    if (!(element is DataFormField field) || field.Key == null)
                     {
-                        model[prop] = b;
+                        continue;
+                    }
+
+                    if (field.Metadata.TryGetValue("sets", out var sets))
+                    {
+                        var children = sets?.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (children != null && children.Length != 0)
+                        {
+                            parentChildRelationships[field.Key] = children;
+                            foreach (var child in children)
+                            {
+                                childParentRelationships[child] = field.Key;
+                            }
+                        }
                     }
                 }
             }
