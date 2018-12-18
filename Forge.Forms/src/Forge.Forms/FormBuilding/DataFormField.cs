8c664455f294b276f8cc6007f1ef12cb1e102b89
Edon Gashi <edon.gashi@uni-pr.edu>
2018-12-18 22:27:53 +0100

    IsNotReadonly computed boolean

--- Forge.Forms/src/Forge.Forms/FormBuilding/DataFormField.cs
+++ Forge.Forms/src/Forge.Forms/FormBuilding/DataFormField.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Forge.Forms.DynamicExpressions;
+using Forge.Forms.DynamicExpressions.BooleanExpressions;
 using Forge.Forms.Validation;
 
 namespace Forge.Forms.FormBuilding
@@ -58,19 +59,24 @@ namespace Forge.Forms.FormBuilding
             }
 
             Resources.Add(nameof(IsReadOnly), IsReadOnly ?? LiteralValue.False);
-            if (IsReadOnly == null)
+            var isEnabled = IsEnabled ?? LiteralValue.True;
+            var isReadonly = IsReadOnly ?? LiteralValue.False;
+            var op = new AndOperator
             {
-                Resources.Add(isNotReadOnly, LiteralValue.True);
-            }
-            else if (IsReadOnly is LiteralValue literal)
-            {
-                Resources.Add(isNotReadOnly, new LiteralValue(literal.Value is false));
-            }
-            else
-            {
-                Resources.Add(isNotReadOnly, IsReadOnly.Wrap("Negate"));
-            }
+                Left = new ValueNode
+                {
+                    Index = 0
+                },
+                Right = new NotOperator
+                {
+                    Child = new ValueNode
+                    {
+                        Index = 1
+                    }
+                }
+            };
 
+            Resources.Add(isNotReadOnly, new MultiBooleanBinding(op, new[] { isEnabled, isReadonly }, null));
             Resources[nameof(DefaultValue)] = DefaultValue ?? LiteralValue.Null;
             Resources.Add(nameof(SelectOnFocus), SelectOnFocus ?? LiteralValue.True);
         }
