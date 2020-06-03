a29f738146ded231a25375084e9abb1328ce4009
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-06-03 02:24:35 -0300

    Adds BoolToVisibilityConverter

---
+++ Forge.Forms.Collections/src/Forge.Forms.Collections/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Forge.Forms.Collections.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool valueAsBool)
+            {
+                var invert = parameter is bool inverted && inverted;
+                
+                if (invert)
+                    valueAsBool = !valueAsBool;
+                
+                return valueAsBool ? Visibility.Visible : Visibility.Collapsed;
+            }
+            
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
