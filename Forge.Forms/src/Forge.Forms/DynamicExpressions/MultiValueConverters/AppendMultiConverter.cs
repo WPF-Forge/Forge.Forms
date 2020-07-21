28512f23077ef7612f029774a4e06364f9e9b579
Alden Alexander <aalexander@bachelorcontrols.com>
2020-07-21 00:41:57 -0500

    Add support for MultiBindings with syntax: '{MultiBinding Value1 Value2|Multiply:0.00}'. The bindings added to the MultiBinding will be separated by a space. Added some example MultiValueConverters (Multiply, Divide, and Append (which can take a parameter for a delimiter)).

---
+++ Forge.Forms/src/Forge.Forms/DynamicExpressions/MultiValueConverters/AppendMultiConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Forge.Forms.DynamicExpressions.MultiValueConverters
+{
+    public class AppendMultiConverter : IMultiValueConverter
+    {
+        public AppendMultiConverter()
+        {
+        }
+
+        public AppendMultiConverter(object delimeter)
+        {
+            Delimeter = delimeter;
+        }
+
+        public object Delimeter { get; }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Delimeter != null)
+            {
+                return string.Join(Delimeter.ToString() ?? string.Empty, values);
+            }
+            else
+            {
+                var sb = new StringBuilder();
+                foreach (var val in values)
+                {
+                    sb.Append(val);
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Forge.Forms/src/Forge.Forms/DynamicExpressions/MultiValueConverters/MultiplyMultiConverter.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Forge.Forms.DynamicExpressions.MultiValueConverters
+{
+    public class MultiplyMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 &&
+                values[0] != null &&
+                values[1] != null)
+            {
+                if (values[0] is decimal dc1)
+                {
+                    if (values[1] is decimal dc2)
+                    {
+                        return dc1 * dc2;
+                    }
+                    else if (values[1] is double d2)
+                    {
+                        return dc1 * (decimal)d2;
+                    }
+                    else if (values[1] is float f2)
+                    {
+                        return dc1 * (decimal)f2;
+                    }
+                    else if (values[1] is long l2)
+                    {
+                        return dc1 * l2;
+                    }
+                    else if (values[1] is int i2)
+                    {
+                        return dc1 * i2;
+                    }
+                    else if (values[1] is short s2)
+                    {
+                        return dc1 * s2;
+                    }
+                }
+                else if (values[0] is double d1)
+                {
+                    if (values[1] is decimal dc2)
+                    {
+                        return d1 * (double)dc2;
+                    }
+                    else if (values[1] is double d2)
+                    {
+                        return d1 * d2;
+                    }
+                    else if (values[1] is float f2)
+                    {
+                        return d1 * f2;
+                    }
+                    else if (values[1] is long l2)
+                    {
+                        return d1 * l2;
+                    }
+                    else if (values[1] is int i2)
+                    {
+                        return d1 * i2;
+                    }
+                    else if (values[1] is short s2)
+                    {
+                        return d1 * s2;
+                    }
+                }
+                else if (values[0] is float f1)
+                {
+                    if (values[1] is decimal dc2)
+                    {
+                        return f1 * (double)dc2;
+                    }
+                    else if (values[1] is double d2)
+                    {
+                        return f1 * d2;
+                    }
+                    else if (values[1] is float f2)
+                    {
+                        return f1 * f2;
+                    }
+                    else if (values[1] is long l2)
+                    {
+                        return f1 * l2;
+                    }
+                    else if (values[1] is int i2)
+                    {
+                        return f1 * i2;
+                    }
+                    else if (values[1] is short s2)
+                    {
+                        return f1 * s2;
+                    }
+                }
+                else if (values[0] is long l1)
+                {
+                    if (values[1] is decimal dc2)
+                    {
+                        return l1 * (double)dc2;
+                    }
+                    else if (values[1] is double d2)
+                    {
+                        return l1 * d2;
+                    }
+                    else if (values[1] is float f2)
+                    {
+                        return l1 * f2;
+                    }
+                    else if (values[1] is long l2)
+                    {
+                        return l1 * l2;
+                    }
+                    else if (values[1] is int i2)
+                    {
+                        return l1 * i2;
+                    }
+                    else if (values[1] is short s2)
+                    {
+                        return l1 * s2;
+                    }
+                }
+                else if (values[0] is int i1)
+                {
+                    if (values[1] is decimal dc2)
+                    {
+                        return i1 * (double)dc2;
+                    }
+                    else if (values[1] is double d2)
+                    {
+                        return i1 * d2;
+                    }
+                    else if (values[1] is float f2)
+                    {
+                        return i1 * f2;
+                    }
+                    else if (values[1] is long l2)
+                    {
+                        return i1 * l2;
+                    }
+                    else if (values[1] is int i2)
+                    {
+                        return i1 * i2;
+                    }
+                    else if (values[1] is short s2)
+                    {
+                        return i1 * s2;
+                    }
+                }
+                else if (values[0] is short s1)
+                {
+                    if (values[1] is decimal dc2)
+                    {
+                        return s1 * (double)dc2;
+                    }
+                    else if (values[1] is double d2)
+                    {
+                        return s1 * d2;
+                    }
+                    else if (values[1] is float f2)
+                    {
+                        return s1 * f2;
+                    }
+                    else if (values[1] is long l2)
+                    {
+                        return s1 * l2;
+                    }
+                    else if (values[1] is int i2)
+                    {
+                        return s1 * i2;
+                    }
+                    else if (values[1] is short s2)
+                    {
+                        return s1 * s2;
+                    }
+                }
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
